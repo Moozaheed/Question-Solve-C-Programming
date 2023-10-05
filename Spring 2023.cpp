c08f84142051817fc7699a34f1e299aee2b046f0
Moozaheed <57921369+Moozaheed@users.noreply.github.com>
2023-10-05 21:03:25 +0600

    Ok

---
+++ Spring 2023.cpp
@@ -0,0 +1,17 @@
+//Ans to the Question No: 3(A)
+
+#include<stdio.h>
+int main()
+{
+    int arr[12]={0};
+
+    arr[0]=1;
+    arr[1]=4;
+    arr[2]=7;
+    arr[3]=10;
+
+    for(int i=0;i<12;i++)
+    {
+        printf("%d ",arr[i]);
+    }
+}
